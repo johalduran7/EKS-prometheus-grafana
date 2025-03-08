a6b4c4c94c2e37b358e32c1cbc65ce6ec0aa4d7e
John Duran <johalduran@gmail.com>
2025-03-08 15:04:22 -0500

    Configured EKS

---
+++ terraform_eks/main.tf
@@ -0,0 +1,67 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "subnet_az_a" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = ["${var.aws_region}a"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_node_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.eks_subnet.id]
+  }
+}
+
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "single-node"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.eks_subnet.id]
+  instance_types  = ["t3.medium"]
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+  disk_size = 16
+}
---
+++ terraform_eks/variables.tf
@@ -0,0 +1,6 @@
+variable aws_region {
+    default="us-east-1"    
+}
+variable eks_cluster_name {
+    default= "eks-cluster"
+}
